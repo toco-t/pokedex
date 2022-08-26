5b59185bd6bb7dd90ad2aa708657af5a7ba29793
toco <ttachibana@my.bcit.ca>
2022-08-26 11:48:12 -0700

    Chore: complete initial setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000...");
+});
