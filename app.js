c26c9ad9b28f429dd3d6fd288244dd1ff1524e0d
toco <ttachibana@my.bcit.ca>
2022-08-26 14:34:04 -0700

    Style: apply colours and set layouts

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+	res.render("landing");
+});
+
 app.listen(3000, () => {
 	console.log("Server is running on port 3000...");
 });
