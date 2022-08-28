579d1782e4ddeb01c2e038b5f39bc5518aabaae6
toco <ttachibana@my.bcit.ca>
2022-08-27 17:49:45 -0700

    Feat: display random pokemon

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+const { getRandomPokemon } = require("./source/js/landing.js");
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.render("landing");
+	getRandomPokemon().then((value) => {
+		res.render("landing", {pokeList: value});
+	});
 });
 
 app.listen(3000, () => {
