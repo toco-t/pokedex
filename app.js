2c42c9ad96a3cfbf2bb94bebba10f50421e7d1b9
toco <ttachibana@my.bcit.ca>
2022-08-28 22:46:19 -0700

    Feat: fetch stats and render card

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const { getRandomPokemon } = require("./source/js/landing.js");
+const { getStats } = require("./source/js/card.js");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -12,7 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	getRandomPokemon().then((value) => {
-		res.render("landing", {pokeList: value});
+		res.render("landing", { pokeList: value });
+	});
+});
+
+app.get("/cards/:id", (req, res) => {
+	getStats(req.params.id).then((value) => {
+		console.log(value);
+		res.render("card", { stats: value });
 	});
 });
 
