687da92221f270910783c2fba2f73c111dc19b99
toco <ttachibana@my.bcit.ca>
2022-08-29 02:20:52 -0700

    Debug: render correct info and apply appropriate background

--- source/js/card.js
+++ source/js/card.js
@@ -1,5 +1,4 @@
 const axios = require("axios").default;
-const _ = require("lodash");
 
 async function getStats(id) {
 	let pokemonInfo = {};
@@ -10,13 +9,12 @@ async function getStats(id) {
 			pokemonInfo = {
 				id: response.data.id,
 				sprite: response.data.sprites.versions["generation-v"]["black-white"].animated.front_default,
-				name: _.startCase(_.toLower(response.data.name)),
+				name: response.data.name,
 				types: response.data.types,
 				height: response.data.height,
 				weight: response.data.weight,
 				stats: response.data.stats,
-				ability_1: _.startCase(_.toLower(response.data.abilities[0].ability.name)),
-				ability_2: _.startCase(_.toLower(response.data.abilities[1].ability.name)),
+				abilities: response.data.abilities
 			};
 		})
 		.catch((error) => {
